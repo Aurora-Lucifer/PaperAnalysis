5286b14a991dfa3d8cceedb272e940956affdb6d
“Aurora-Lucifer” <yimingz@zju.edu.cn>
2021-12-20 16:57:23 +0800

    查询功能实现(暂不支持存储)

--- analysis.py
+++ analysis.py
@@ -6,8 +6,9 @@
 '''
 
 import re
+import os
 import json
-from utils import read_all, save_all, path_generate
+from utils import database_generate, read_all, save_all, path_generate
 
 '''
 data_path -> accept paper list 所在的html页面【暂无爬虫手动复制】
@@ -47,10 +48,11 @@ UseLessWords = [
 
 
 key_words = [
-    "active",
-    "active learning",
-    "selection",
-    "acquisition"
+    "Curriculum",
+    # "active",
+    # "active learning",
+    # "selection",
+    # "acquisition"
 ]
 
 
@@ -113,11 +115,72 @@ def count_analysis(conference, year, word_lenth):
     json.dump(count, open(analysis_result, 'w'), indent=-1)
 
 
+def title_selection(key_words: list, title_list: list):
+    '''
+        筛选title_list中含key_words的项
+        输入两个list
+        返回一个dict，key_words为键，对应选中的title列表为值
+    '''
+    ret = {}
+    for key in key_words:
+        ret[key] = []
+        for title in title_list:
+            if key in title:
+                ret[key].append(title)
+    return ret
 
-# def query_for_keywords():
-#     pass
+
+def database_selection(conference_list=None, year_range=None):
+    '''
+        根据会议范围和年份范围选择需要的会议
+            缺损状态默认全选
+        返回一个 dict 键为"会议-年份"，值为title路径
+    '''
+    ret = {}
+    database_list = os.listdir('dataset')
+    for folder in database_list:
+        if '_' in folder and '.' not in folder:
+            conference, year = folder.split('_')
+            # 判断会议和年份是否正确
+            # 如果为None, 默认正确
+            if (
+                conference_list is None or conference in conference_list
+            ) and (
+                year_range is None or year in year_range
+            ):
+                target_path = 'dataset/' + folder
+                file_list = os.listdir(target_path)
+                for file_name in file_list:
+                    if 'title.txt' in file_name:
+                        target_path += '/' + file_name
+                        ret[conference+'-'+year] = target_path
+    return ret
+
+
+def query_for_keywords(key_words, conference_list=None, year_range=None):
+    title_paths = database_selection(conference_list, year_range)
+    ret = {}
+    # 查询
+    for words in key_words:
+        ret[words] = {}
+    for target in title_paths.keys():
+        titles = read_all(title_paths[target])
+        query_ans = title_selection(key_words, titles)
+        for words in key_words:
+            ret[words][target] = query_ans[words]
+
+    # 输出
+    for words in key_words:
+        print(words)
+        for target in ret[words].keys():
+            if len(ret[words][target]) != 0:
+                print(target)
+                for title in ret[words][target]:
+                    print(title, end='')
+
+    return ret
 
 
 if __name__ == "__main__":
     # count_analysis(conference_name, year_number, 3)
-
+    ans = query_for_keywords(key_words)
